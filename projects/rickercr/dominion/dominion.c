08fa3ce724affacc6014830d9c2536d6cc7029d7
craigricker <craigd.ricker@gmail.com>
2019-01-26 19:56:32 -0800

    Working adventuer refactor

--- projects/rickercr/dominion/dominion.c
+++ projects/rickercr/dominion/dominion.c
@@ -642,6 +642,28 @@ int getCost(int cardNumber)
 	
   return -1;
 }
+int adventurer_turn(int z, int drawntreasure, int currentPlayer, int temphand[],
+                    int cardDrawn, struct gameState * state ) {
+   while(drawntreasure<2){
+      if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+         shuffle(currentPlayer, state);
+      }
+      drawCard(currentPlayer, state);
+      cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+      if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+         drawntreasure++;
+      else{
+         temphand[z]=cardDrawn;
+         state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+         z++;
+      }
+   }
+   while(z-1>=0){
+      state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+      z=z-1;
+   }
+   return 0;
+}
 
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
@@ -667,25 +689,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+       return adventurer_turn(z, drawntreasure, currentPlayer, temphand,
+                              cardDrawn, state);
 			
     case council_room:
       //+4 Cards
